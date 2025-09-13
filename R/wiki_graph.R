12e95c5a0ac4b6a3848f80c718cccaf6d1849755
Finn Beckmann <fbeckmann.edu@pm.me>
2025-09-13 21:16:15 +0200

    add dijkstra algorithm

---
+++ R/wiki_graph.R
@@ -0,0 +1,11 @@
+#' Example graph in edge list format (**Wikipedia Dijkstra example**)
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{Node label (source node) — integer}
+#'   \item{v2}{Node label (target node) — integer}
+#'   \item{w}{Edge weight (non-negative numeric)}
+#' }
+#'
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+"wiki_graph"
