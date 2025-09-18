a45722e9f325dc1f6a067a61f4801f1f92d5c3c3
Finn Beckmann <fbeckmann.edu@pm.me>
2025-09-18 12:15:23 +0200

    rename euclidean to euclid

--- R/euclid.R
+++ R/euclid.R
@@ -6,7 +6,7 @@
 #' @param y numeric scalar or integer
 #' @return greatest common divisor of x and y
 #' @export
-euclidean <- function(x, y) {
+euclid <- function(x, y) {
 
   stopifnot(is.numeric(x) || is.integer(x))
   stopifnot(is.numeric(y) || is.integer(y))
